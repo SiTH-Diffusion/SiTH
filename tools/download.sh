6fbfbfb08359ea04ec76f0453289a44f13fe2a0d
azuxmioy <azuxmioy@gmail.com>
2024-04-14 18:50:19 +0200

    Add files via upload

---
+++ tools/download.sh
@@ -0,0 +1,2 @@
+wget https://files.ait.ethz.ch/projects/SiTH/recon_model.pth -O checkpoints/recon_model.pth
+wget https://files.ait.ethz.ch/projects/SiTH/save_smplerx.pth -O checkpoints/save_smplerx.pth
