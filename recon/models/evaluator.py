cd90396c94655b93f241adedf045a3656daefff8
azuxmioy <azuxmioy@gmail.com>
2024-05-16 19:24:00 +0200

    Update an option to generate meshes w/ UV textures

--- recon/models/evaluator.py
+++ recon/models/evaluator.py
@@ -1,14 +1,13 @@
-"""
-Copyright (C) 2024  ETH Zurich, Hsuan-I Ho
-"""
-
 import torch
 import torch.nn as nn
 import time
 import trimesh
 import os
 import logging as log
-
+import cv2
+import numpy as np
+import nvdiffrast.torch as dr
+from PIL import Image
 from kaolin.ops.conversions import voxelgrids_to_trianglemeshes
 from kaolin.ops.mesh import subdivide_trianglemesh
 
@@ -25,6 +24,8 @@ class Evaluator(nn.Module):
 
         # Set device to use
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.glctx = dr.RasterizeCudaContext(device=self.device)
+
         #device_name = torch.cuda.get_device_name(device=self.device)
         #log.info(f'Using {device_name} with CUDA v{torch.version.cuda}')
 
@@ -59,7 +60,34 @@ class Evaluator(nn.Module):
         self.tex_model.to(self.device)
         
 
-    def test_reconstruction(self, data, save_path, subdivide=True, chunk_size=1e5, flip=False):
+    def _repair_mesh(self, mesh):
+
+        # remove disconnect par of mesh
+        connected_comp = mesh.split(only_watertight=False)
+        max_area = 0
+        max_comp = None
+        for comp in connected_comp:
+            if comp.area > max_area:
+                max_area = comp.area
+                max_comp = comp
+        mesh = max_comp
+            
+        trimesh.repair.fix_inversion(mesh)
+
+        return mesh
+
+    def _uv_padding(self, image, hole_mask):
+        inpaint_image = (
+            cv2.inpaint(
+                (image.detach().cpu().numpy() * 255).astype(np.uint8),
+                (hole_mask.detach().cpu().numpy() * 255).astype(np.uint8),
+                4,
+                cv2.INPAINT_TELEA,
+                )
+            )
+        return inpaint_image
+
+    def test_reconstruction(self, data, save_path, subdivide=True, chunk_size=1e5, flip=False, save_uv=False):
 
         fname = data['fname'][0]
         log.info(f"Reconstructing mesh for {fname}...")
@@ -98,41 +126,97 @@ class Evaluator(nn.Module):
             vertices, faces = subdivide_trianglemesh(vertices, faces, iterations=1)
 
         # Next estimate the texture rgb values on the surface
-        _points = torch.split(vertices, int(chunk_size), dim=1)
-        front_feat, back_feat = self.tex_model.compute_feat_map(front_rgb_img, back_rgb_img)
-        pred_rgb = []
-        for _p in _points:
-            output = self.tex_model.compute_rgb(_p, front_feat, back_feat, smpl_v, vis_class)
-            pred_rgb.append(output)
+        if save_uv:
+            
+            d = trimesh.Trimesh(vertices=vertices[0].cpu().detach().numpy(), 
+                faces=faces.cpu().detach().numpy(), 
+                process=False)
+            
+            d = self._repair_mesh(d)
 
-        pred_rgb = torch.cat(pred_rgb, dim=1)
-        
-        h = trimesh.Trimesh(vertices=vertices[0].cpu().detach().numpy(), 
-             faces=faces.cpu().detach().numpy(), 
-             vertex_colors=pred_rgb[0].cpu().detach().numpy(),
-             process=False)
-            
-        # remove disconnect par of mesh
-        connected_comp = h.split(only_watertight=False)
-        max_area = 0
-        max_comp = None
-        for comp in connected_comp:
-            if comp.area > max_area:
-                max_area = comp.area
-                max_comp = comp
-        h = max_comp
-            
-        trimesh.repair.fix_inversion(h)
-        
-        if flip: # flip to the gradio coordinate system
-            h.apply_transform( [[-1, 0, 0, 0],
+            import xatlas
+
+            vmap, uv_faces, uvs = xatlas.parametrize(d.vertices, d.faces)
+            faces_tensor = torch.from_numpy(uv_faces.astype(np.int32)).to(self.device)
+
+            uv_clip = torch.from_numpy(uvs) * torch.tensor([2.0, -2.0]) + torch.tensor([-1.0, 1.0])
+            # pad to four component coordinate
+            uv_clip4 = torch.cat(
+                (
+                    uv_clip,
+                    torch.zeros_like(uv_clip[..., 0:1]),
+                    torch.ones_like(uv_clip[..., 0:1]),
+                ),
+                dim=-1,
+            ).to(self.device)
+            # rasterize
+            rast, _ = dr.rasterize(self.glctx, uv_clip4[None,...], faces_tensor, (1024, 1024), grad_db=True)
+            #rast0 = torch.flip(rast[0], dims=(1,))
+            rast0 = rast[0]
+            hole_mask = ~(rast0[:, :, 3] > 0)
+            gb_pos, _ = dr.interpolate(torch.from_numpy(d.vertices).to(self.device).float(), rast, torch.from_numpy(d.faces).to(self.device).int())
+            gb_pos = gb_pos[0].view(1,-1, 3)
+            _points = torch.split(gb_pos, int(chunk_size), dim=1)
+
+            front_feat, back_feat = self.tex_model.compute_feat_map(front_rgb_img, back_rgb_img)
+            pred_rgb = []
+            for _p in _points:
+                output = self.tex_model.compute_rgb(_p, front_feat, back_feat, smpl_v, vis_class)
+                pred_rgb.append(output)
+
+            pred_rgb = torch.cat(pred_rgb, dim=1).view(1024, 1024, 3)
+
+            pad_rgb = self._uv_padding(pred_rgb, hole_mask)
+
+            texture_map = Image.fromarray(pad_rgb)
+
+            h = trimesh.Trimesh(
+                vertices=d.vertices[vmap],
+                faces=uv_faces,
+                visual=trimesh.visual.TextureVisuals(uv=uvs, image=texture_map),
+                process=False,
+            )
+
+            if flip: # flip to the gradio coordinate system
+                h.apply_transform( [[-1, 0, 0, 0],
                                 [0, 1, 0, 0],
                                  [0, 0, 1, 0],
                                  [0, 0, 0, 1]] )
-            
+
+            h.visual.material.name = fname
+            obj_path = os.path.join(save_path, '%s_reco.obj' % (fname))
+            h.export(obj_path, mtl_name=fname+'.mtl')
+
+            with open(os.path.join(save_path, fname+'.mtl'), 'w') as f:
+                f.write('newmtl {}\n'.format(fname))
+                f.write('map_Kd {}.png\n'.format(fname))
+
+        else:
+            _points = torch.split(vertices, int(chunk_size), dim=1)
+            front_feat, back_feat = self.tex_model.compute_feat_map(front_rgb_img, back_rgb_img)
+            pred_rgb = []
+            for _p in _points:
+                output = self.tex_model.compute_rgb(_p, front_feat, back_feat, smpl_v, vis_class)
+                pred_rgb.append(output)
+
+            pred_rgb = torch.cat(pred_rgb, dim=1)
         
-        obj_path = os.path.join(save_path, '%s_reco.obj' % (fname))
-        h.export(obj_path)
+            h = trimesh.Trimesh(vertices=vertices[0].cpu().detach().numpy(), 
+                faces=faces.cpu().detach().numpy(), 
+                vertex_colors=pred_rgb[0].cpu().detach().numpy(),
+                process=False)
+
+            if flip: # flip to the gradio coordinate system
+                h.apply_transform( [[-1, 0, 0, 0],
+                                [0, 1, 0, 0],
+                                 [0, 0, 1, 0],
+                                 [0, 0, 0, 1]] )
+
+
+            h = self._repair_mesh(h)
+            obj_path = os.path.join(save_path, '%s_reco.obj' % (fname))
+            h.export(obj_path)
+
         end = time.time()
         log.info(f"Reconstruction finished in {end-start} seconds.")
         
